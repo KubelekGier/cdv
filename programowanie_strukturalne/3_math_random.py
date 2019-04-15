9c718a1e67a039c26fd21736ed4e36506afbb66d
Bartosz Wackowski <bartosz.wackowski0@gmail.com>
2019-04-15 14:56:47 +0200

    Update 3_math_random.py

--- programowanie_strukturalne/3_math_random.py
+++ programowanie_strukturalne/3_math_random.py
@@ -7,8 +7,14 @@ print(pi)
 #pierwiastek
 pierwiastek = math.sqrt(9)
 print(pierwiastek)
-
-
+#modol random
+import random
+losuj = random.random()
+print(losuj)
+print(f'Zmienna wylosowana:{losuj}')
+losujzlisty=random.choise([1,2,3,4])
+print(f'wyloswana'(losujzlisty))
 
 
 print()
+
